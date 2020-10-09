f9c7ef33ade226a59390a675b5f7d620b2351d96
imabotplayer <72506325+imabotplayer@users.noreply.github.com>
2020-10-09 22:13:27 +0200

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -9,6 +9,7 @@
 // @match        https://agar.io/*
 // @downloadURL  https://imabotplayer.github.io/proudmod-agar/main.user.js
 // @updateURL    https://imabotplayer.github.io/proudmod-agar/main.user.js
+// @require      https://imabotplayer.github.io/proudmod-agar/PMexpress.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @grant        GM_registerMenuCommand
