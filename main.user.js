23f330dc20b72ba651182f342417df2e017bc087
imabotplayer <72506325+imabotplayer@users.noreply.github.com>
2020-10-09 22:05:37 +0200

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Proudmod for Agar.io
+// @namespace    Proudmod for Agar.io
+// @version      0.1
+// @description  Agario Mod
+// @homepage     https://imabotplayer.github.io/proudmod-agar
+// @author       Imabotplayer
+// @license      MIT
+// @match        https://agar.io/*
+// @downloadURL  https://imabotplayer.github.io/proudmod-agar/main.user.js
+// @updateURL    https://imabotplayer.github.io/proudmod-agar/main.user.js
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+
+// ==/UserScript==
