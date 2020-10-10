c1616a06c377f120bac6a08a063a204ac3e20244
imabotplayer <72506325+imabotplayer@users.noreply.github.com>
2020-10-10 10:51:50 +0200

    Update adblock.js

--- tools/adblock.js
+++ tools/adblock.js
@@ -1,1 +1,11 @@
+let ads = [
+	document.querySelector("#adsTop"),
+	document.querySelector("#adsBottom"),
+	document.querySelector("#adsLeft"),
+	document.querySelector("#adsRight"),
+	document.querySelector("#videoContainer")
+];
 
+for(let i = 0; i > ads.length; i++) {
+	ads[i].innerHTML = null;
+}
